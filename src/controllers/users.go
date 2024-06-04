4d2e82ef08a7b18cda7e57e30b5ff06258c3af99
Felipe <nemmanc@protonmail.com>
2024-06-03 23:39:09 -0300

    add: controllers

---
+++ src/controllers/users.go
@@ -0,0 +1,23 @@
+package controllers
+
+import "net/http"
+
+func FindUsers(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func FindUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+
+}
