bf3426c41bbc4e2ec950a9ed45befd8a543a399f
Felipe <nemmanc@protonmail.com>
2024-06-04 21:07:29 -0300

    add: controllers-comments

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -2,22 +2,37 @@ package controllers
 
 import "net/http"
 
+// FindUsers Controller
+//
+// Find all users in the database
 func FindUsers(w http.ResponseWriter, r *http.Request) {
 
 }
 
+// FindUser Controller
+//
+// Find a specific user in the database
 func FindUser(w http.ResponseWriter, r *http.Request) {
 
 }
 
+// CreateUser Controller
+//
+// Creates a new user in the database
 func CreateUser(w http.ResponseWriter, r *http.Request) {
 
 }
 
+// UpdateUser Controller
+//
+// Updates a specific user information in the database
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
 
 }
 
+// DeleteUser Controller
+//
+// Delete a specific user
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
 
 }
